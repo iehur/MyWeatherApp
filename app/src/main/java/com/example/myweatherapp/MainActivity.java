86fcf5c6625ab2275573a0fe3e14d0fd7dc1b76b
Ila Hur <seraph.ihur@gmail.com>
2019-05-15 12:46:35 -0400

    minor fix

--- app/src/main/java/com/example/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/myweatherapp/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showErrorDialog(String message) {
+        loadingIcon.setVisibility(View.GONE);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder
             .setMessage(message)
